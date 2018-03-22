19cd2ae219336d95b238dfb0d4d5cde95f477d39
Irina <ira.ezdakova@gmail.com>
2018-03-22 16:44:32 +0300

    files for project 2

--- site/untitled.js
+++
@@ -1,8 +0,0 @@
-
-    <script>
-    document.write("<h2>Числа Фибоначчи </h2>");
-    for (i=0, j=1, k=0, fib =0; i<50; i++, fib=j+k, j=k, k=fib){
-        document.write("Fibonacci (" + i + ") = " + fib);
-        document.write("<br>");
-    }
-	</script>
