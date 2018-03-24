b51c48f246a72c62aa3f115fd1d404093dbc63ba
Irina <ira.ezdakova@gmail.com>
2018-03-24 09:09:44 +0300

    JS Homework 1.1

---
+++ javascript/Task 5.js
@@ -0,0 +1,17 @@
+var a = getRandom(1, 6);
+var b = getRandom(1, 6);
+var c = getRandom(1, 6);
+var result = isSamePair(a, b, c);
+console.log(a, b, c);
+console.log(result);
+
+
+function getRandom(min, max) {
+	var random = Math.random() * (max - min) + min;
+	random = Math.floor(random);
+	return random;
+} 
+
+function isSamePair(a, b, c){
+	return a === b || a === c || b === c;
+}
