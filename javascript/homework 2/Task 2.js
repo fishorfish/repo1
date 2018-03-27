777bbad680ee55ebff95275dd1cbb58c3199feb8
Irina <ira.ezdakova@gmail.com>
2018-03-27 15:24:28 +0300

    fixed task 2

--- javascript/homework 2/Task 2.js
+++ javascript/homework 2/Task 2.js
@@ -1,27 +1,30 @@
 var pointA = {};
-pointA.x = 0;
-pointA.y = 0;
+pointA.x = parseInt(prompt("Введите координату вершины А по оси х"));
+pointA.y = parseInt(prompt("Введите координату вершины А по оси y"));
+pointA.z = parseInt(prompt("Введите координату вершины А по оси z"));
 
 var pointB = {};
-pointB.x = 0;
-pointB.y = 3;
+pointB.x = parseInt(prompt("Введите координату вершины B по оси х"));
+pointB.y = parseInt(prompt("Введите координату вершины B по оси y"));
+pointB.z = parseInt(prompt("Введите координату вершины B по оси z"));
 
 var pointC = {};
-pointC.x = 4;
-pointC.y = 0;
+pointC.x = parseInt(prompt("Введите координату вершины C по оси х"));
+pointC.y = parseInt(prompt("Введите координату вершины C по оси y"));
+pointC.z = parseInt(prompt("Введите координату вершины C по оси z"));
 
 console.log(isRightTriangular(pointA, pointB, pointC));
 
 function isRightTriangular(pointA, pointB, pointC) {
-	var a = getLength(pointA, pointB);
-	var b = getLength(pointB, pointC);
-	var c = getLength(pointC, pointA);
+	var a = getLengthInPow2(pointA, pointB);
+	var b = getLengthInPow2(pointB, pointC);
+	var c = getLengthInPow2(pointC, pointA);
 
- 	return Math.pow(a, 2) ===  Math.pow(b, 2) + Math.pow(c, 2) || 
-	Math.pow(b, 2) ===  Math.pow(a, 2) + Math.pow(c, 2) ||
-	Math.pow(c, 2) ===  Math.pow(a, 2) + Math.pow(b, 2);
+ 	return a === b + c || 
+		b === a + c || 
+		c === a + b;
 }
 
-function getLength(pointA, pointB) {
-	return Math.sqrt(Math.pow((pointB.x - pointA.x), 2) + Math.pow((pointB.y - pointA.y), 2));
+function getLengthInPow2(pointA, pointB) {
+	return Math.pow((pointB.x - pointA.x), 2) + Math.pow((pointB.y - pointA.y), 2) + Math.pow((pointB.z - pointA.z), 2);
 }
