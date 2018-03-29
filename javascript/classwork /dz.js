18175ecc92dad7f5e5917705eb0d2cc2a1766910
Irina <ira.ezdakova@gmail.com>
2018-03-29 16:06:47 +0300

    delete classwork from javascript

--- javascript/classwork /dz.js
+++
@@ -1,8 +0,0 @@
-
-var oklad = prompt("Введите количество оклада");
-var days = prompt("Введите количество отработанных дней");
-var hours = prompt("Введите количество отработанных часов");
-var g = parseFloat(oklad) / hours * days * (0,87);
-
-alert(g.toFixed(2));
-
--- javascript/classwork /dz6.js
+++
@@ -1,9 +0,0 @@
-var num = prompt("Введите номер телефона");
-var reg = /7 \(\d{3}\) \d{3}-\d{2}-\d{2}/;
-alert(reg.test(num));
-
-
-var email = prompt("Введите email");
-var reg = /[a-z-0-9_.-]+@[a-z-0-9.-]+\.[a-z]+/;
-alert(reg.test(email));
-
