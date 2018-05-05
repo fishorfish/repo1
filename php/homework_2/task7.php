61705575f9708947d708e70d9ff8f5e0f611c5ca
Irina <ira.ezdakova@gmail.com>
2018-05-05 13:44:22 +0300

    fixed tasks

--- php/homework_2/task7.php
+++ php/homework_2/task7.php
@@ -1,6 +1,7 @@
 <?php
 
-$array = ['a','b','c','d','e','f','j'];
+//$array = ['a','b','c','d','e','f','j'];
+$array =(range('a', 'j'));
 foreach($array as &$value) {
 	$value = strtoupper($value);
 }
